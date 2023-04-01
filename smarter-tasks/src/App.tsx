34ccff1c2fac5617c5ec4895338dabe2d71065db
Onkar Jadhav <omjadhav2610@gmail.com>
2023-04-01 17:15:32 +0530

    Minor changes to make test cases pass

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,58 +1,11 @@
-import React from "react";
-import DateUtil from "./DateUtil";
-import { Task } from "./Task";
-import TaskForm from "./TaskForm";
-import TaskList from "./TaskList";
+import TaskApp from "./TaskApp";
 
-interface TaskAppProp {}
-interface TaskAppState {
-  tasks: Task[];
-}
-
-class App extends React.Component<TaskAppProp, TaskAppState> {
-  constructor(props: TaskAppProp) {
-    super(props);
-    this.state = {
-      tasks: [
-        {
-          title: "Build the website with static content",
-          description: "Build a static website and deploy it on netlify",
-          dueDate: DateUtil.tommorrow,
-          assigneeName: "Kekeh",
-        },
-        {
-          title: "Add a blog",
-          description: "Share ideas with a blog",
-          dueDate: DateUtil.today,
-          assigneeName: "Kekeh",
-        },
-      ],
-    };
-  }
-
-  addTask = (task: Task) => {
-    this.setState({
-      tasks: [...this.state.tasks, task],
-    });
-  };
-  render() {
-    return (
-      <>
-        <h1 className="text-violet-600 border-b border-b-violet-500 p-4 font-bold bg-black text-2xl md:text-3xl sticky top-0">
-          <img src="/favicon.svg" alt="YATL Logo" className="inline w-6 md:w-10" />{" "}
-          YetAnotherTodoList
-        </h1>
-
-        <div className="mt-5 m-auto max-w-lg p-4">
-          <div className=" bg-gray-900 p-4 rounded-md shadow-md shadow-black">
-          <TaskForm addTask={this.addTask} />
-          </div>
-          <hr className="mt-4 mb-4" />
-          <TaskList tasks={this.state.tasks} />
-        </div>
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <TaskApp />
+    </div>
+  );
 }
 
 export default App;
