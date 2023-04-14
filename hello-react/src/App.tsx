7eddb40dabbe3874508572a7785dd74b2d3061eb
Onkar Jadhav <omjadhav2610@gmail.com>
2023-04-14 09:05:11 +0530

    Bot tweak

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,5 +1,24 @@
 import TaskCard from "./TaskCard"
-import DateUtil from "./DateUtil"
+class DateUtil {
+  static _oneday = 60 * 60 * 24 * 1000;
+
+  static format(date : Date) {
+    return date.toLocaleDateString("en-CA");
+  }
+
+  static get today() {
+    return new Date();
+  }
+
+  static get yesterday() {
+    return new Date(this.today.getTime() - this._oneday);
+  }
+
+  static get tommorrow() {
+    return new Date(this.today.getTime() + this._oneday);
+  }
+}
+
 
 function App() {
   const pendingTasks = [{
