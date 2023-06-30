dadbbd69998351f66b04c2f62217c2b3186fcd7a
Onkar Jadhav <omjadhav2610@gmail.com>
2023-06-30 19:24:26 +0530

    Remove redundant index.html

--- smarter-tasks/vite.config.js
+++ smarter-tasks/vite.config.js
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
   return defineConfig({
-    build:{
-      rollupOptions: {
-        input : 'src/index.tsx',
-      }
-    },
     plugins: [react()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
