2b47efb7c62ae12acd1ec70bf2e9900970d4d992
Onkar Jadhav <omjadhav2610@gmail.com>
2023-06-30 18:47:22 +0530

    Add missing files

---
+++ smarter-tasks/src/layouts/account/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Appbar from "./Appbar"
+import {Outlet} from "react-router-dom";
+
+const AccountLayout = () => {
+  return (
+    <main>
+      <Appbar/>
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <Outlet/>
+      </div>
+    </main>
+  )
+}
+
+export default AccountLayout
